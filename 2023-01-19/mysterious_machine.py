2b5f8d8880750863922b3b8679ed3a9055c0ce68
High Point University C.O.D.E. Club <code@highpoint.edu>
2023-01-16 17:03:59 -0500

    Update mysterious_machine.py

--- 2023-01-19/mysterious_machine.py
+++ 2023-01-19/mysterious_machine.py
@@ -1,6 +1,22 @@
+# BEWARE SPOOKY GHOSTS!!
+#
+#                      .-.
+#         heehee      /aa \_
+#                   __\-  / )                 .-.
+#         .-.      (__/    /        haha    _/oo \
+#       _/ ..\       /     \               ( \v  /__
+#      ( \  u/__    /       \__             \/   ___)
+#       \    \__)   \_.-._._   )  .-.       /     \
+#       /     \             `-`  / ee\_    /       \_
+#    __/       \               __\  o/ )   \_.-.__   )
+#   (   _._.-._/     hoho     (___   \/           '-'
+#    '-'                        /     \
+#                             _/       \    teehee
+#                            (   __.-._/
+
 import caesar
 
-KEY = 0b0101010 & 0x42 | 0x44 >> 2
+KEY = 0b0101010 & 0x42 | 0x44 >> 2  # nothing scarier than bit math
 
 def encipher(plaintext: str):
         return caesar.encipher(plaintext, KEY)
